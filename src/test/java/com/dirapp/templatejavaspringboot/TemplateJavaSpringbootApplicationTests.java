3f40057312bd5873ea8c66576067f974503582c5
dirasanjayawardana <dirawardana.dw@gmail.com>
2024-03-07 06:29:40 +0700

    backup

---
+++ src/test/java/com/dirapp/templatejavaspringboot/TemplateJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dirapp.templatejavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateJavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
